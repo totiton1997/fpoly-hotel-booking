fbba87782439f8f65e69e39a8d69dd59fe369b27
thaond1205 <thaond1205@gmail.com>
2021-10-12 00:01:49 +0700

    thaond code done hien thi co so

---
+++ hotel-management/src/main/java/com/fpt/hotel/service/IFacilitiesService.java
@@ -0,0 +1,10 @@
+package com.fpt.hotel.service;
+
+import com.fpt.hotel.model.Facility;
+
+import java.util.List;
+
+public interface IFacilitiesService {
+
+    List<Facility> findAllFacilities();
+}
